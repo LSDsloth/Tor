ac621a59bbf691acd101176d53281e41a6a96be9
LSDsloth <lst1788@gmail.com>
2024-09-15 00:15:25 +0200

    Added new songs

--- scripts/songs.js
+++ scripts/songs.js
@@ -5,9 +5,27 @@ export const songs = [
         track: "../sang/sang-1.mp4"
     },
     {
-        name: "Tor Volume.2",
+        name: "666",
         image: "../bilder/bilde-2.jpg",
-        track: "../sang/sang-1.mp4"
+        track: "../sang/666.mp3"
 
-    }
+    },
+    {
+        name: "PLUG (feat.KaashKD)",
+        image: "../bilder/bilde-2.jpg",
+        track: "../sang/plug.mp3"
+
+    },
+    {
+        name: "FLY AV STED",
+        image: "../bilder/bilde-2.jpg",
+        track: "../sang/fly-av-sted.mp3"
+
+    },
+    {
+        name: "De Som Vet De Vet (freestyle feat. inkvivo, trenrik, millioscho)",
+        image: "../bilder/bilde-2.jpg",
+        track: "../sang/de-som-vet-de-vet.mp3"
+
+    },
 ]
